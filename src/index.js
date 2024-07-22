ae519a99554e5b57eb973258fac12a2312f4db6d
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-07-22 22:08:59 +0700

    update package.json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthProvider";
 import { CartProvider } from "./components/Cart/CartContext";
 import { OrderProvider } from "./context/OrderProvider";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
